630e4ab8a13cd6ca5c1210d903ad76d225c83aee
Ashley Plasket <ashley.plasket@gmail.com>
2023-03-29 11:32:00 -0400

    fix: add def initialize local variable  @contract_length = contract_length

--- lib/player.rb
+++ lib/player.rb
@@ -3,7 +3,7 @@ class Player
   def initialize(name, monthly_cost, contract_length)
     @name = name
     @monthly_cost = monthly_cost
-    @contract_length
+    @contract_length = contract_length
   end
 
   def first_name

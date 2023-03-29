8865c4da6973340994664fad50e45fad530436c3
Ashley Plasket <ashley.plasket@gmail.com>
2023-03-29 11:30:34 -0400

    fix: was only commiting spec file - adding this commit to match spec commits. feat: add player class, def initialize, first_name and last_name methods

---
+++ lib/player.rb
@@ -0,0 +1,17 @@
+class Player
+  attr_reader :name, :monthly_cost, :contract_length
+  def initialize(name, monthly_cost, contract_length)
+    @name = name
+    @monthly_cost = monthly_cost
+    @contract_length
+  end
+
+  def first_name
+    @name.split(" ").first
+  end
+
+  def last_name
+    @name.split(" ").last
+  end
+
+end

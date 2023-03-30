57a9b1fd020955bd60c87b1ac3b32b1c4ecfb8e7
Ashley Plasket <ashley.plasket@gmail.com>
2023-03-30 12:52:26 -0400

    erase comments

--- lib/player.rb
+++ lib/player.rb
@@ -1,4 +1,3 @@
-#refactored code:
 class Player
   attr_reader :first_name, :last_name, :monthly_cost, :contract_length, :nickname
   def initialize(name, monthly_cost, contract_length)
@@ -18,29 +17,3 @@ class Player
   end
 end
 
-##original code:
-# class Player
-#   attr_reader :name, :monthly_cost, :contract_length, :nickname
-#   def initialize(name, monthly_cost, contract_length)
-#     @name = name
-#     @monthly_cost = monthly_cost
-#     @contract_length = contract_length
-#     @nickname = nickname
-#   end
-
-#   def first_name
-#     @name.split(" ").first
-#   end
-
-#   def last_name
-#     @name.split(" ").last
-#   end
-
-#   def total_cost
-#     @contract_length * @monthly_cost
-#   end
-
-#   def set_nickname!(new_nickname)
-#     @nickname = new_nickname
-#   end
-# end

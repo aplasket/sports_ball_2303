35d26b81e9297c5c0dfab948bffb0bf25543b8aa
Ashley Plasket <ashley.plasket@gmail.com>
2023-03-30 12:48:18 -0400

    refactor player file with first and last name as instance variables

--- lib/player.rb
+++ lib/player.rb
@@ -1,18 +1,12 @@
+#refactored code:
 class Player
-  attr_reader :name, :monthly_cost, :contract_length, :nickname
+  attr_reader :first_name, :last_name, :monthly_cost, :contract_length, :nickname
   def initialize(name, monthly_cost, contract_length)
-    @name = name
+    @first_name = name.split.first
+    @last_name = name.split.last
     @monthly_cost = monthly_cost
     @contract_length = contract_length
     @nickname = nickname
-  end
-
-  def first_name
-    @name.split(" ").first
-  end
-
-  def last_name
-    @name.split(" ").last
   end
 
   def total_cost
@@ -23,3 +17,30 @@ class Player
     @nickname = new_nickname
   end
 end
+
+##original code:
+# class Player
+#   attr_reader :name, :monthly_cost, :contract_length, :nickname
+#   def initialize(name, monthly_cost, contract_length)
+#     @name = name
+#     @monthly_cost = monthly_cost
+#     @contract_length = contract_length
+#     @nickname = nickname
+#   end
+
+#   def first_name
+#     @name.split(" ").first
+#   end
+
+#   def last_name
+#     @name.split(" ").last
+#   end
+
+#   def total_cost
+#     @contract_length * @monthly_cost
+#   end
+
+#   def set_nickname!(new_nickname)
+#     @nickname = new_nickname
+#   end
+# end
